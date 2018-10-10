5bf1a530520a4ebd5074fea96000f8dccded1eb3
Mario Mishev <mario.mishev@egt-bg.com>
2018-10-10 17:03:52 +0300

    Add SDL_Init

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "SDL2/SDL.h"
 
 int main(int argc, char* args[])
 {
+	if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0)
+	{
+		//TODO add log information
+		return -1;
+	}
 
+	SDL_Quit();
+	return 0;
 }
